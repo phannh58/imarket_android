04650dce8ead6795667ba8dd311785527b924b5c
Lan <vulan238@gmail.com>
2016-12-29 09:21:26 +0700

    fix crash in comment screen and category screen

--- app/src/main/java/com/example/framgia/imarketandroid/util/findpath/InternetUtil.java
+++ app/src/main/java/com/example/framgia/imarketandroid/util/findpath/InternetUtil.java
@@ -1,13 +1,8 @@
 package com.example.framgia.imarketandroid.util.findpath;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.widget.Toast;
-
-import com.example.framgia.imarketandroid.R;
 
 /**
  * Created by phongtran on 23/09/2016.
@@ -15,19 +10,17 @@ import com.example.framgia.imarketandroid.R;
 public class InternetUtil {
     public static boolean isInternetConnected(Context context) {
         ConnectivityManager connectivityManager =
-            (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         boolean result = false;
         if (connectivityManager != null) {
             NetworkInfo networkInfo =
-                connectivityManager.getActiveNetworkInfo();
+                    connectivityManager.getActiveNetworkInfo();
             if (networkInfo != null
-                && (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE
-                || networkInfo.getType() == ConnectivityManager.TYPE_WIFI)) {
+                    && (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE
+                    || networkInfo.getType() == ConnectivityManager.TYPE_WIFI)) {
                 result = true;
             }
         }
-//        if (!result)
-//            Toast.makeText(context, R.string.no_internet, Toast.LENGTH_SHORT).show();
         return result;
     }
 }
